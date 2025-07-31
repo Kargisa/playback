c34846ad9ad22f1888074dd5d68cae559437e0d0
Jan540 <naj.schaefer@gmail.com>
2025-07-31 22:51:36 +0200

    animations manager + refined movement

---
+++ Scripts/player_animation_manager.gd
@@ -0,0 +1,99 @@
+extends AnimatedSprite2D
+
+@export var player_node: CharacterBody2D
+
+enum PlayerState {
+	IDLE,
+	RUN,
+	CHARGING,
+	JUMP,
+	FALLING,
+	SPLAT,
+	HURT,
+	DIE
+}
+
+var current_state: PlayerState = PlayerState.IDLE
+var player_is_charging: bool = false
+var is_playing_splat: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play_animation(current_state)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if not player_node:
+		return
+	
+	if player_node.velocity.x != 0:
+			flip_h = player_node.velocity.x > 0
+	
+	if is_playing_splat:
+		return
+	
+	if player_node.is_on_floor():
+		if player_is_charging:
+			return
+		
+		if player_node.velocity.x != 0:
+			set_state(PlayerState.RUN)
+		else:
+			set_state(PlayerState.IDLE)	
+	else:
+		if player_node.velocity.y < 0:
+			set_state(PlayerState.JUMP)
+		elif player_node.velocity.y > 0:
+			set_state(PlayerState.FALLING)
+	
+func _on_player_started_charging() -> void:
+	player_is_charging = true
+	if not is_playing_splat and player_node.is_on_floor(): 
+		set_state(PlayerState.CHARGING)
+
+func _on_player_stopped_charging() -> void:
+	player_is_charging = false
+
+func _on_player_landed_on_ground() -> void:
+	is_playing_splat = true
+	set_state(PlayerState.SPLAT)
+
+func _on_animation_finished() -> void:
+	if current_state == PlayerState.SPLAT:
+		is_playing_splat = false
+		if player_is_charging:
+			set_state(PlayerState.CHARGING)
+		elif player_node.velocity.x != 0:
+			set_state(PlayerState.RUN)
+		else:
+			set_state(PlayerState.IDLE)
+
+func set_state(new_state):
+	if current_state == new_state:
+		return 
+		
+	current_state = new_state
+	play_animation(current_state)
+
+func play_animation(state: PlayerState):
+	var anim_name = "IDLE"
+	
+	match state:
+		PlayerState.IDLE:
+			anim_name = "IDLE"
+		PlayerState.RUN:
+			anim_name = "RUN"
+		PlayerState.CHARGING:
+			anim_name = "CHARGING"
+		PlayerState.JUMP:
+			anim_name = "JUMP"
+		PlayerState.FALLING:
+			anim_name = "FALLING"
+		PlayerState.SPLAT:
+			anim_name = "SPLAT"
+		PlayerState.HURT:
+			anim_name = "HURT"
+		PlayerState.DIE:
+			anim_name = "DIE"
+	
+	play(anim_name)
--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,29 +1,65 @@
 extends CharacterBody2D
 
 @export var SPEED : float = 80.0
-@export var JUMP_VELOCITY : float = -100.0
+@export var SPEED_MULT_WHILE_CHARGING : float = 0.5
+@export var SPEED_MULT_WHILE_IN_AIR : float = 1.2
+@export var BASE_JUMP_VELOCITY : float = -100.0
+@export var MAX_JUMP_VELOCITY: float = -350.0
 @export var pushForce : float = 80.0
+@export var CHARGING_SPEED : float = 1.0
 
-@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+signal started_charging
+signal stopped_charging
+signal landed_on_ground
 
+var is_charging_jump = false
+var jump_velocity = 0.0
+var is_grounded = false
 
 func _physics_process(delta: float) -> void:
 	if $RecordingManager.isReverting:
 		return
+		
+	var actual_speed = SPEED
 	
-	# Add the gravity.
-	if not is_on_floor():
+	if not is_grounded and is_on_floor():
+		landed_on_ground.emit()
+	
+	is_grounded = is_on_floor()
+	
+	# Add the gravity.                               
+	if !is_grounded:
 		velocity += get_gravity() * delta
+		actual_speed *= SPEED_MULT_WHILE_IN_AIR
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		started_charging.emit()
+		is_charging_jump = true
+		jump_velocity = BASE_JUMP_VELOCITY
+		
+	if is_charging_jump and jump_velocity > MAX_JUMP_VELOCITY:
+		jump_velocity -= 5 * CHARGING_SPEED
 
+	if Input.is_action_just_released("jump"):
+		stopped_charging.emit()
+		is_charging_jump = false
+		
+		if is_grounded:
+			velocity.y = jump_velocity
+		
+		
+	
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	if direction:
-		velocity.x = direction * SPEED
-		sprite.flip_h = direction > 0
+		
+		if !is_grounded:
+			actual_speed *= SPEED_MULT_WHILE_IN_AIR
+		elif is_charging_jump :
+			actual_speed *= SPEED_MULT_WHILE_CHARGING
+		
+		velocity.x = direction * actual_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
