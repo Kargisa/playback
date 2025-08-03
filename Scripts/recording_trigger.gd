cb35304866b0840c4592837101d6e9342b163770
Jan540 <naj.schaefer@gmail.com>
2025-08-03 13:59:35 +0200

    working fr this time...

--- Scripts/recording_trigger.gd
+++ Scripts/recording_trigger.gd
@@ -1,9 +1,8 @@
 extends Node2D
 class_name RecordingTrigger
 
-var recording_manager : RecordingManager
+var recording_manager : RecordingManager 
 @export var recording_id : int
 
 func _on_recording_button_on_button_pressed() -> void:
-	print(recording_manager)
 	recording_manager.start_recording(recording_id)
