ab660ca0e2b4f1aa1077b385f75d43583be214eb
Jan540 <naj.schaefer@gmail.com>
2025-08-03 13:22:41 +0200

    merged successfully !=)(/Z$§=§()/&Z(E= OIUSHF =)/(ZZ

---
+++ Scripts/recording_button.gd
@@ -0,0 +1,78 @@
+extends Node2D
+
+class_name RecordingButton
+
+@export var releaseDelay : float = 0.0
+var timeSinceAreaExited : float = 0.0
+var areaExited = false
+var nodesOnButton : int = 0
+
+@onready var area : Area2D = $Area2D
+@onready var sprite : Sprite2D = $Sprite2D
+
+signal onButtonPressed
+signal onButtonReleased
+
+func pressButton() -> void:
+	sprite.frame = 1
+	AudioManager.play_sound(AudioResource.SoundEffect.BUTTON_PRESSED)
+	onButtonPressed.emit()
+
+func releaseButton() -> void:
+	sprite.frame = 0
+	AudioManager.play_sound(AudioResource.SoundEffect.BUTTON_RELEASED)
+	onButtonReleased.emit()
+	
+
+func onAreaEntered(node : Node) -> void:
+	if node.is_in_group("player"):
+		nodesOnButton += 1
+		if nodesOnButton == 1:
+			pressButton()
+	
+
+func onAreaExited(node : Node) -> void:
+	if node.is_in_group("player"):
+		nodesOnButton -= 1
+		if nodesOnButton == 0:
+			timeSinceAreaExited = 0.0
+			areaExited = true
+		
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area.body_entered.connect(onAreaEntered)
+	area.body_exited.connect(onAreaExited)
+	
+func _process(delta: float) -> void:
+	if not areaExited:
+		return
+	
+	if timeSinceAreaExited < releaseDelay:
+		timeSinceAreaExited += delta
+		return
+	
+	areaExited = false
+	releaseButton()
+	pass
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- Scripts/recording_trigger.gd
+++ Scripts/recording_trigger.gd
@@ -4,9 +4,5 @@ class_name RecordingTrigger
 @export var recording_manager : RecordingManager
 @export var recording_id : int
 
-func _on_button_on_button_pressed() -> void:
+func _on_recording_button_on_button_pressed() -> void:
 	recording_manager.start_recording(recording_id)
-
-func _on_button_on_button_released() -> void:  
-	pass
-	
