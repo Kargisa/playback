89a89091212325f8cb2bd2a6d9b1000d315f530b
Jan540 <naj.schaefer@gmail.com>
2025-08-03 18:32:47 +0200

    wtf is happening

--- Scripts/game_ui.gd
+++ Scripts/game_ui.gd
@@ -6,7 +6,7 @@ class_name GameUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+
 	GameManager.restartProgressBar = $GameUI/RestartBrogressBar
 	($GameUI/JumpChargeBar as JumpChargeBar).player = player
 	($GameUI/TextureRect as PocketWatch).recording_manager = recording_manager
@@ -14,9 +14,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var tintColor : Color = Color(1.0, 1.0, 1.0, 0.0)
-	
-	tintColor.a = clamp(GameManager.timeSinceRestartHeld / GameManager.timeToHoldRestart * 2.0, 0.0, 1.0) 
-	
-	
+
+	tintColor.a = clamp(GameManager.timeSinceRestartHeld / GameManager.timeToHoldRestart * 2.0, 0.0, 1.0)
+
+
 	GameManager.restartProgressBar.tint_progress = tintColor
 	GameManager.restartProgressBar.tint_under = tintColor
